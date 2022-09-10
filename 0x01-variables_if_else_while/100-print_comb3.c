5bad99703236ad852e4585365bdfff859bc4c619
TeemahLwittySwiffyFwarry <oladosuoluwayemisi@gmail.com>
2022-09-09 19:41:38 -0700

    task 10

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -11,25 +11,27 @@
 
 int main(void)
 {
-	int digit1, digit2;
+	int digi1, digit2;
 
-	for (digit1 =0; digit1 < 9; digit++)
+	for (digit1 = 48; digit1 < 58; digit1++)
 	{
-		for (digit2 = digit1 + 1; digit2 < 10; digit2++)
+		for (digit2 = 49; digit2 < 58; digit2++)
 		{
-			putchar((digit1 % 10) + '0');
-			putchar((digit2 % 10) + '0');
+			if (digit2 > digit1)
+			{
+				putchar(digit1);
+				putchar(digit2);
 
-			if (digit1 == 8 && digit2 == 9)
-			continue;
-
-			putchar(',');
-			putchar(' ');
+				if (digit1 != 56 || digit2 != 57)
+				{
+					putchar(44);
+					putchar(32);
+				}
+			}
 		}
 	}
-	putchar('\n');
+
+	putchar('n\');
 
 	return (0);
-
 }
-		    
