97c3e35861f98f6157d815dafacab04463608b09
TeemahLwittySwiffyFwarry <oladosuoluwayemisi@gmail.com>
2022-09-13 08:05:03 -0700

    Task 0 commit

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,28 @@
+#include <main.h>
+
+/**
+ * main - Entry point
+ *
+ * Description:Writing a program that
+ * print putchar followed by a new line.
+ *
+ * Return 0 (Success)
+ *
+ */
+
+int main(void)
+{
+	putchar('_');
+	putchar('p');
+	putchar('u');
+	putchar('t');
+	putchar('c');
+	putchar('h');
+	putchar('a');
+	putchar('r');
+
+	putchar('\n')
+
+	return (0);
+
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+This program is basically for nested loop
