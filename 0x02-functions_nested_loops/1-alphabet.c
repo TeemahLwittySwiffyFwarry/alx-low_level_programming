0d20cb8f76feb593d30d73666837b53ef9f57fda
TeemahLwittySwiffyFwarry <oladosuoluwayemisi@gmail.com>
2022-09-13 14:29:25 -0700

    task 2

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print alphabet -> prints the lowercase alphabets
+ */
+
+void print_alphabet(void)
+{
+	int j;
+
+	for (j = 'a'; j <= 'z'; j++)
+	{
+		_putchar(j);
+	}
+	_putchar('\n');
+}
