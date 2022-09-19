e304438f4781bbd8f1e50e86043e9d86aece212d
TeemahLwittySwiffyFwarry <oladosuoluwayemisi@gmail.com>
2022-09-19 14:12:52 -0700

    other task

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -30,5 +30,5 @@ int _atoi(char *s)
 		c++;
 	}
 	ni *= min;
-	return (ni)
+	return (ni);
 }
--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -2,9 +2,9 @@
 
 /**
  * _puts - prints a string
- * 
+ *
  * @str: the string
- * 
+ *
  * Return: the length of the string
  */
 
@@ -13,7 +13,9 @@ void _puts(char *str)
 	int i = 0;
 
 	while (str[i] != '\0')
-	{_putchar(str[i]);
+	{
+		_putchar(str[i]);
 		i++;
-	}_putchar('\n');
+	}
+	_putchar('\n');
 }
